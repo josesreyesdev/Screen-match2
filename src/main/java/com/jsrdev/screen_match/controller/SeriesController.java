0a4e2016405f0f16947850d409bd8d29848d7adc
josesreyesdev <josesanchez09190@gmail.com>
2025-06-24 01:13:43 -0600

    added return a series list

--- src/main/java/com/jsrdev/screen_match/controller/SeriesController.java
+++ src/main/java/com/jsrdev/screen_match/controller/SeriesController.java
@@ -1,13 +1,23 @@
 package com.jsrdev.screen_match.controller;
 
+import com.jsrdev.screen_match.model.Series;
+import com.jsrdev.screen_match.repository.SeriesRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SeriesController {
 
+    private final SeriesRepository seriesRepository;
+
+    public SeriesController(SeriesRepository seriesRepository) {
+        this.seriesRepository = seriesRepository;
+    }
+
     @GetMapping("/series")
-    public String getString() {
-        return "Hello World, it is my first message";
+    public List<Series> getSeries() {
+        return seriesRepository.findAll();
     }
 }
